69aa09cf0988290506e01f0c7e8a4a1b59ae2914
Franco Bruno <francobrunounicen@gmail.com>
2026-06-15 11:25:43 -0300

    Delete TPE_web2 directory

--- TPE_web2/app/models/home.model.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class HomeModel {
-    private $db;
-
-    public function __construct (){
-        $this->db = new PDO ('mysql:host=localhost;dbname=catalogo_bbva;charset=utf8', 'root', '');
-    }
-}
--- TPE_web2/app/views/error.view.php
+++
@@ -1,8 +0,0 @@
-<?php
-class ErrorView {
-    public function renderError(string $error) {
-        // el template error.php puede acceder a las mismas variables 
-        // que tiene alcance en esta función, por ejemplo $error
-        require_once __DIR__ . '/../../templates/error.php';
-    }
-}
